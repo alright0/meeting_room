67ac7692901cfb732bb167fd8f30425024c331d0
alright0 <koro6@yandex.ru>
2021-06-15 08:21:23 +0300

    данные для pg

--- tst/settings.py
+++ tst/settings.py
@@ -80,24 +80,17 @@ CHANNEL_LAYERS = {"default": {"BACKEND": "channels.layers.InMemoryChannelLayer"}
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if DEBUG == True:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "meeting_room",
+        "USER": "user0",
+        "PASSWORD": "user0",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
-            "NAME": "meeting_room",
-            "USER": "user0",
-            "PASSWORD": "user0",
-            "HOST": "127.0.0.1",
-            "PORT": "5432",
-        }
-    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
